8b9cf6e23d1280354be5b8568f293cb9b5cba928
hassonquadri <30322270+hassonquadri@users.noreply.github.com>
2017-08-02 18:23:41 -0400

    sh script

---
+++ packerdemo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ARTIFACT=`packer build -machine-readable packer-demo.json | awk -F, '$0 ~/artifact,0,id/ {print $6}'`
+AMI_ID=`echo $ARTIFACT |cut -d ':' -f2`
+echo 'variable "APP_INSTANCE_AMI" { default = "'${AMI_ID}'" }' > amivar.tf
+aws s3 cp amivar.tf s3://terraform-state-12345/amivar.tf
